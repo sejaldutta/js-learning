911554cd3526a39bf299462abe8b8b436fc904af
sejaldutta <sejaldutta2004@gmail.com>
2025-10-09 06:11:42 +0000

    910

--- .devcontainer/01_basics/array.js
+++ .devcontainer/01_basics/array.js
@@ -1,4 +1,38 @@
-const Arr1 = [3,4,5]
-console.log(Arr1[1])
-Arr1.push(7)
-console.log(Arr1)
+const Arr1 = [3,4,5,7,2,4,9]
+// console.log(Arr1[1])
+// console.log(typeof Arr1)
+// Arr1.push(7)
+// console.log(Arr1)
+// Arr1.pop()
+// console.log(Arr1)
+// Arr1.unshift(8)
+// console.log(Arr1)
+// Arr1.shift()
+// console.log(Arr1)
+// console.log(Arr1.indexOf(5))
+// console.log(typeof (Arr1.join()))
+// console.log(Arr1.includes(8))
+
+// *****Slice and splice*****
+// console.log(Arr1.slice(1,3))
+// console.log (Arr1)
+// console.log(Arr1.splice(1,3))
+// console.log(Arr1)
+
+// const Arr2=[1,2,3,4,5]
+// const Arr3=['thor', 'yoda', 'doctor Strange']
+// // const Arr4 = Arr2.concat(Arr3)
+// // console.log(Arr4)
+// const Arr5=['fish','prawns','pork']
+// const Arr6=[...Arr1,...Arr2,...Arr3,...Arr5]
+// console.log(Arr6)
+// const Arr7=[1,2,3,[4,5],[6,[7,8],9]]
+// console.log(Arr7.flat(Infinity))
+
+console.log(Array.isArray("Sejal"))
+console.log(Array.from("SEJAL"))
+console.log(Array.from({name: "Sejal"}))
+const s1=100
+const s2=200
+const s3=300
+console.log(Array.of(s1, s2, s3))
