7c64a5164ce39c9b10075143c6e1cb2bb1395ee4
sejaldutta <sejaldutta2004@gmail.com>
2025-10-11 09:25:32 +0000

    11102

--- .devcontainer/01_basics/function.js
+++ .devcontainer/01_basics/function.js
@@ -3,9 +3,42 @@
 // }
 // const result = addNum(3,4)
 // console.log("Result: ",result)
-function addNum(num1,num2){
-    let result = num1 + num2
-    return result
+// function addNum(num1,num2){
+//     let result = num1 + num2
+//     return result
+// }
+// const result = addNum(3,4)
+// console.log(result) 
+
+// function isPresent(name){
+//     if(name===undefined){
+//         console.log("please enter valid username")
+//         return
+//     }
+//     else
+//         return `${name} is present`
+// }
+// console.log(isPresent("sejal"))
+
+// function cartvalue(num2,...num1){
+//     return (num2, num1);
+// }
+// console.log(cartvalue(200, 250, 360, 550))
+
+// ***function in object***
+// const Details={
+//     username: "sejal",
+//     password: 1234,
+//     place:"jamshedpur"
+// }
+// function Accesspoint(object1){
+//     console.log(`password of ${object1.username} is ${object1.password} lives in ${object1.place}`)
+// }
+// Accesspoint(Details)
+
+// *func in array *
+const arr1=[100, 200, 300, 400]
+function myarr(anyarray){
+    console.log(anyarray[2])
 }
-const result = addNum(3,4)
-console.log(result) 
+myarr(arr1)
