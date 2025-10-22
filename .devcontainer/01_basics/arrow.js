6cd497c38bd3f1c2c6bc17f98821f5cb7ce13997
sejaldutta <sejaldutta2004@gmail.com>
2025-10-22 09:22:28 +0000

    2110

---
+++ .devcontainer/01_basics/arrow.js
@@ -0,0 +1,40 @@
+// const user = {
+// username: "Sejal",
+// price: 1,
+// welcome: function(){
+//     console.log(`${this.username}, welcomes all`)
+// }
+// }
+// user.welcome()
+// user.username = "Sejal Dutta"
+// user.welcome()
+
+
+
+// const user = {
+// username: "Sejal",
+// price: 1,
+// welcome: function(){
+//     console.log(`${this.username}, welcomes all`)
+// //     console.log(this)
+// }
+// }
+// // user.welcome()
+// console.log(this)
+
+// const user = function(){
+//     let username= "Sejal"
+//     console.log(this);
+// }
+// user()
+
+// const chai= () => {
+//     let username= "Sejal"
+//     console.log(this);
+// }
+// chai()
+
+const addution = (num1, num2) =>{
+    return (num1 + num2);
+}
+console.log(addution(70, 80))
