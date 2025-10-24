55b4b06e3ac63a8414cc29f0dcb92dbf6acb7c8a
sejaldutta <sejaldutta2004@gmail.com>
2025-10-24 10:52:20 +0000

    21101

---
+++ .devcontainer/01_basics/flow.js
@@ -0,0 +1,25 @@
+// const username = function(){}
+// if(username){
+//     console.log("valid")
+// }
+// else
+//     console.log("invalid")
+
+// const Email = []
+// if(Email.length===0){
+//     console.log("empty")
+// }
+// else{
+//     console.log("not empty")
+// }
+
+// const user = {}
+// if(Object.keys(user).length === 0){
+//     console.log("empty")
+// }
+// else
+//     console.log("notempty")
+
+// Nullish Coalescing
+let val1 = undefined??10
+console.log(val1)
---
+++ .devcontainer/01_basics/loop.js
@@ -0,0 +1,40 @@
+// for (let i = 1; i<=5; i++){
+//     console.log("multiple of", i)
+//     for(let j=1; j<=10; j++){
+//         console.log(`${i}x${j}=${i*j}`)
+//     }
+// }
+
+// const myArr = ["Sejal", "Sakshi","Sneha","Yashi","Seema" ]
+// console.log(myArr.length)
+// for( let i = 0; i<(myArr.length); i++){
+//     console.log(myArr[i])
+// }
+
+
+// for(let i=1; i<=10; i++){
+//     if(i==5){
+//         console.log("5 detected")
+//         continue
+//     }
+//     console.log(i)
+
+// for(let i=1; i<=10; i++){
+//     if(i==5){
+//         console.log("5 detected")
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// let i = 0
+// while(i<=10){
+//     console.log(i)
+//     i+=2
+// }
+
+let j=11
+do{
+    console.log(j)
+    j+=2
+}while(j<=10);
