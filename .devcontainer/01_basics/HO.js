a5ab7014ce06f3d7676d9cc9cee2564320ceb7c7
sejaldutta <sejaldutta2004@gmail.com>
2025-10-25 11:55:49 +0000

    25101

---
+++ .devcontainer/01_basics/HO.js
@@ -0,0 +1,16 @@
+// const Arr= [1,2,3,4,5]
+// for (const mem of Arr) {
+//     console.log(mem)
+// }
+
+const country = new Map()
+country.set('IN','INDIA')
+country.set('IN','INDIA')
+country.set('JA','JAPAN')
+country.set('CH','CHINA')
+country.set('IR','IRAN')
+console.log(country)
+
+// for (const [key, value] of country){
+//     console.log(key, "=>", value)
+// }
