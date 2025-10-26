530208d59c1a973520e40d8afa25b4cd48748e25
sejaldutta <sejaldutta2004@gmail.com>
2025-10-26 12:37:35 +0000

    21102

--- .devcontainer/01_basics/filter.js
+++ .devcontainer/01_basics/filter.js
@@ -6,10 +6,17 @@
 
 // mynum.push(8)
 // console.log(mynum)
-const newnum=[]
-mynum.forEach((num)=>{
-    if(num<4){
-newnum.push(num)
-    }
-})
+// const newnum=[]
+// mynum.forEach((num)=>{
+//     if(num<4){
+// newnum.push(num)
+//     }
+// })
+// console.log(newnum)
+
+//  const num1 =[]
+// // mynum.forEach((num)=> num1.push(num+10))
+// // console.log(num1)
+// or use map
+const newnum = mynum.map((num)=>(num+10))
 console.log(newnum)
